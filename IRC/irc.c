88f01a63a71fe0d3089ad648b3b979d66e46e881
Rahul Jaitly <rahulj@uchicago.edu>
2021-09-07 18:44:37 -0500

    projects added

---
+++ IRC/irc.c
@@ -0,0 +1,7 @@
+● Coded multi-threaded server to provide users with concurrent access
+● Read about socket code to use set up server access
+● Programmed 15 commands for users to communicate with each other in sophisticated ways
+
+
+All Code was in C. I cannot post the code in a public repository per the UChicago honor code.
+If you are a company wanting to see this code, please email me, and I can share it with you.
---
+++ Router/router.c
@@ -0,0 +1,7 @@
+● Implemented simple router to forward IPv4 datagrams via two router and three router topologies
+● Added ICMP, ARP, and IP forwarding mechanisms
+● Used mutexes to program an ARP cache to deal with standby ARP requests          
+                                                                                
+                                                                                
+All Code was in C. I cannot post the code in a public repository per the UChicago honor code.
+If you are a company wanting to see this code, please email me, and I can share it with you.
